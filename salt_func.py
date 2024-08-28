3c9fd33d309d2ccd8cf9a3017fd0796b667ab72d
Weifeng Zhong <weifeng@weifengzhong.com>
2024-08-28 07:57:57 -0400

    sd exercise w/ salt marriage penalty

--- salt_func.py
+++ salt_func.py
@@ -1,14 +1,23 @@
 import pandas as pd
 from taxbrain.taxbrain import TaxBrain
 
-def calc_rev_diff(single_amount, sub, cg, sd_adjust=0, rate_adjust=0):
+def calc_rev_diff(single_amount, sub, cg, sd_adjust=0, rate_adjust=0, with_penalty=False):
     tcja_ext = "github://OFRA-ORG:Tax-Calculator-thru74@tcja/taxcalc/reforms/ext.json"
-    salt_profile = {
-        "ID_AllTaxes_c": {"2026":  [single_amount, single_amount*2, single_amount, single_amount, single_amount*2]},
-        "STD": {"2026": [15339.57+sd_adjust, 30679.14+sd_adjust*2, 15339.57+sd_adjust, 22979.74+sd_adjust*1.5, 30679.14+sd_adjust*2]},
-        "II_rt1": {"2026": 0.10+rate_adjust},
-        "PT_rt1": {"2026": 0.10+rate_adjust}
-    }
+    if with_penalty:
+        salt_profile = {
+            "ID_AllTaxes_c": {"2026":  [single_amount, single_amount, single_amount/2, single_amount, single_amount]},
+            "STD": {"2026": [15339.57+sd_adjust, 30679.14+sd_adjust*2, 15339.57+sd_adjust, 22979.74+sd_adjust*1.5, 30679.14+sd_adjust*2]},
+            "II_rt1": {"2026": 0.10+rate_adjust},
+            "PT_rt1": {"2026": 0.10+rate_adjust}
+        }
+        .1
+    else:
+        salt_profile = {
+            "ID_AllTaxes_c": {"2026":  [single_amount, single_amount*2, single_amount, single_amount, single_amount*2]},
+            "STD": {"2026": [15339.57+sd_adjust, 30679.14+sd_adjust*2, 15339.57+sd_adjust, 22979.74+sd_adjust*1.5, 30679.14+sd_adjust*2]},
+            "II_rt1": {"2026": 0.10+rate_adjust},
+            "PT_rt1": {"2026": 0.10+rate_adjust}
+        }
     model = TaxBrain(2026, 2035, microdata="TMD", base_policy=tcja_ext, reform=salt_profile, behavior={"sub": sub, "cg": cg})
     model.run()
     result_df = model.weighted_totals("combined") * 1e-12
